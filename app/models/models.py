0bbb72cc29e67ff0c7f8bdb202ffc42f5b44b86d
uec-sampei <s2298013@gl.cc.uec.ac.jp>
2023-09-25 14:15:48 +0900

    models.pyの作成

---
+++ app/models/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from models.database import Base
+from datetime import datetime
+
+
+class Request(Base):
+    __tablename__ = 'requests'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128), unique=True)
+    body = Column(Text)
+    date = Column(DateTime, default=datetime.now())
+
+    def __init__(self, title=None, body=None, date=None):
+        self.title = title
+        self.body = body
+        self.date = date
+
+    def __repr__(self):
+        return '<Title %r>' % (self.title)
