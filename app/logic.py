17db713edb95a5987d3ec88040a306504394e53b
uec-sampei <s2298013@gl.cc.uec.ac.jp>
2023-09-25 11:12:12 +0900

    logic.pyにクエストリングを受け取る機構を追加した

--- app/logic.py
+++ app/logic.py
@@ -1,19 +1,19 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
 
 #「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 @app.route("/")
-def hello():
-    return "Hello World"
+# def hello():
+#     return "Hello World"
 
 #「/index」へアクセスがあった場合に、「index.html」を返す
 @app.route("/index")
 def index():
-    return render_template("index.html")
-
+    name = request.args.get("name")
+    return render_template("index.html", name=name)
 
 #おまじない
 if __name__ == "__main__":
