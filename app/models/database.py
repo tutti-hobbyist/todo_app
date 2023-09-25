b3dfb1536328e66ac8ff323a86a7ebc4a253287a
tutti-hobbyist <tutti.hobbyist@gmail.com>
2023-09-25 18:03:51 +0900

    update db init

--- app/models/database.py
+++ app/models/database.py
@@ -9,7 +9,5 @@ db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=e
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-
 def init_db():
-    import models.model
     Base.metadata.create_all(bind=engine)
